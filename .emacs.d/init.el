339a2460690a0664a3ebfad862137f243ad77080
Peter Danenberg <pcd@roxygen.org>
2013-08-09 00:31:50 -0700

    SQL stuff

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -82,6 +82,28 @@ Then switch to the process buffer."
             (define-key python-mode-map (kbd "C-c z")
               (lambda () (interactive) (python-switch-to-python t)))))
 
+;;; SQL
+(defun my-sql-save-history-hook ()
+  (let ((lval 'sql-input-ring-file-name)
+        (rval 'sql-product))
+    (if (symbol-value rval)
+        (let ((filename
+               (concat "~/.emacs.d/sql/"
+                       (symbol-name (symbol-value rval))
+                       "-history.sql")))
+          (set (make-local-variable lval) filename))
+      (error
+       (format "SQL history will not be saved because %s is nil"
+               (symbol-name rval))))))
+
+(add-hook 'sql-interactive-mode-hook 'my-sql-save-history-hook)
+(add-hook 'sql-mode-hook
+          (lambda ()
+            (define-key sql-mode-map (kbd "TAB") 'sql-indent-line)))
+;;; The executable is osql, but osql doesn't seem to pass things to
+;;; isql correctly.
+(setq sql-ms-options '("--" "-w" "300" "-n"))
+
 ;;; Dired should grep case insensitively.
 (setq find-grep-options "-q -i")
 (add-hook 'dired-mode-hook
