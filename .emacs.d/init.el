3533e863dc116bb8ba99d56e069656272637ea91
Peter Danenberg <danenberg@google.com>
2015-05-28 15:23:33 -0700

    Smarter move to beginning of line

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -81,6 +81,32 @@
 
 ;;;; Miscellaneous
 
+;;; Smarter-move-beginning-of-the-line combines back-to-indentation
+;;; (M-m) and move-beginning-of-line (C-a); from
+;;; <http://goo.gl/2Pr7I8>.
+(defun smarter-move-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+
+Move point to the first non-whitespace character on this line.
+If point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+
+If ARG is not nil or 1, move forward ARG - 1 lines first.  If
+point reaches the beginning or end of the buffer, stop there."
+  (interactive "^p")
+  (setq arg (or arg 1))
+
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+
 ;;; Ace-jump-mode
 
 ;; Everything becomes invisible, otherwise; should we let emacs know
