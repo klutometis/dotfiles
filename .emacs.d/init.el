a95248b48e0cd542b0d2cefc22493de7c0122ee8
Peter Danenberg <danenberg@google.com>
2015-05-30 23:37:36 -0700

    Add smart-open-line and smart-open-line-above.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -81,6 +81,27 @@
 
 ;;;; Miscellaneous
 
+;;; Indent when issuing open-line; see e.g.
+;;; <http://www.emacswiki.org/emacs/OpenNextLine> or
+;;; <http://emacsredux.com/blog/2013/03/26/smarter-open-line/>.
+(defun smart-open-line ()
+  "Insert an empty line after the current line.
+Position the cursor at its beginning, according to the current mode."
+  (interactive)
+  (move-end-of-line nil)
+  (newline-and-indent))
+
+;;; Same thing for the line above; see e.g.
+;;; <http://emacsredux.com/blog/2013/06/15/open-line-above/>.
+(defun smart-open-line-above ()
+  "Insert an empty line above the current line.
+Position the cursor at it's beginning, according to the current mode."
+  (interactive)
+  (move-beginning-of-line nil)
+  (newline-and-indent)
+  (forward-line -1)
+  (indent-according-to-mode))
+
 ;;; Smarter-move-beginning-of-the-line combines back-to-indentation
 ;;; (M-m) and move-beginning-of-line (C-a); from
 ;;; <http://goo.gl/2Pr7I8>.
@@ -469,6 +490,7 @@ Then switch to the process buffer."
 (global-set-key (kbd "C-c C-z u") 'browse-url)
 (global-set-key (kbd "C-c C-z v") 'browse-url-of-file)
 (global-set-key (kbd "C-c M-i") 'helm-multi-swoop)
+(global-set-key (kbd "C-o") 'smart-open-line)
 (global-set-key (kbd "C-h SPC") 'helm-all-mark-rings)
 (global-set-key (kbd "C-x b") 'helm-mini)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
@@ -478,6 +500,7 @@ Then switch to the process buffer."
 (global-set-key (kbd "M-%") 'query-replace-regexp)
 (global-set-key (kbd "M-i") 'helm-swoop)
 (global-set-key (kbd "M-I") 'helm-swoop-back-to-last-point)
+(global-set-key (kbd "M-o") 'smart-open-line-above)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "<up>") 'windmove-up)
