3d61f3d25ffc537e1a8f34d04806585d4c05320f
Peter Danenberg <danenberg@google.com>
2015-11-18 15:21:22 -0800

    Add copy-line.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1158,7 +1158,29 @@ point reaches the beginning or end of the buffer, stop there."
           (set-visited-file-name new-name)
                     (set-buffer-modified-p nil))))))
 
-;;; Load a host-specific file, if one exists.
+;;; Copy the current line, don't kill it; see
+;;; <http://www.emacswiki.org/emacs/CopyingWholeLines> for other
+;;; solutions, also <http://stackoverflow.com/questions/88399>.
+(defun copy-line (arg)
+  "Copy lines (as many as prefix argument) in the kill ring.
+      Ease of use features:
+      - Move to start of next line.
+      - Appends the copy on sequential calls.
+      - Use newline as last char even on the last line of the buffer.
+      - If region is active, copy its lines."
+  (interactive "p")
+  (let ((beg (line-beginning-position))
+        (end (line-end-position arg)))
+    (when mark-active
+      (if (> (point) (mark))
+          (setq beg (save-excursion (goto-char (mark)) (line-beginning-position)))
+        (setq end (save-excursion (goto-char (mark)) (line-end-position)))))
+    (if (eq last-command 'copy-line)
+        (kill-append (buffer-substring beg end) (< end beg))
+      (kill-ring-save beg end)))
+  (kill-append "\n" nil)
+  (beginning-of-line (or (and arg (1+ arg)) 2))
+  (if (and arg (not (= 1 arg))) (message "%d lines copied" arg)))
 (let ((host-file (format "~/.emacs.d/%s.el" system-name)))
   (if (file-exists-p host-file)
       (load host-file)))
