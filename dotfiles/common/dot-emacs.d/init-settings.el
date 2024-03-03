dc6300ba7129ad52a824b347d1a05de2aa489f4a
Peter Danenberg <pcd@roxygen.org>
2024-03-03 09:04:19 +0000

    Emacs: filename to clipboard

--- dotfiles/common/dot-emacs.d/init-settings.el
+++ dotfiles/common/dot-emacs.d/init-settings.el
@@ -1,3 +1,15 @@
+;;; Copy filename to the clipboard; from
+;;; <http://emacsredux.com/blog/2013/03/27/copy-filename-to-the-clipboard/>.
+(defun copy-file-name-to-clipboard ()
+  "Copy the current buffer file name to the clipboard."
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (kill-new filename)
+      (message "Copied buffer file name '%s' to the clipboard." filename))))
+
 ;;; Save all buffers, no confirmation (doesn't seem to work).
 (defun save-all-buffers-no-confirmation (orig-func &rest args)
   "Save all buffers without confirmation."
