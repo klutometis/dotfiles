43722c56514dd5f93d07548fcc8078df7525368d
Peter Danenberg <pcd@roxygen.org>
2024-12-12 19:21:40 -0800

    Make avy actions play well.

--- dotfiles/common/dot-emacs.d/avy-actions.el
+++ dotfiles/common/dot-emacs.d/avy-actions.el
@@ -1,24 +1,24 @@
 ;; Code used in the demos at https://karthinks.com/software/avy-can-do-anything
 ;; Tweak as desired.
 
-(package-install 'avy)
-(setq avy-keys '(?q ?e ?r ?y ?u ?o ?p
-                    ?a ?s ?d ?f ?g ?h ?j
-                    ?k ?l ?' ?x ?c ?v ?b
-                    ?n ?, ?/))
+;; (package-install 'avy)
+;; (setq avy-keys '(?q ?e ?r ?y ?u ?o ?p
+;;                     ?a ?s ?d ?f ?g ?h ?j
+;;                     ?k ?l ?' ?x ?c ?v ?b
+;;                     ?n ?, ?/))
 
 
-(defun avy-show-dispatch-help ()  
+(defun avy-show-dispatch-help ()
   (let* ((len (length "avy-action-"))
          (fw (frame-width))
          (raw-strings (mapcar
-                   (lambda (x)
-                     (format "%2s: %-19s"
-                             (propertize
-                              (char-to-string (car x))
-                              'face 'aw-key-face)
-                             (substring (symbol-name (cdr x)) len)))
-                   avy-dispatch-alist))
+                       (lambda (x)
+                         (format "%2s: %-19s"
+                                 (propertize
+                                  (char-to-string (car x))
+                                  'face 'aw-key-face)
+                                 (substring (symbol-name (cdr x)) len)))
+                       avy-dispatch-alist))
          (max-len (1+ (apply #'max (mapcar #'length raw-strings))))
          (strings-len (length raw-strings))
          (per-row (floor fw max-len))
@@ -30,7 +30,7 @@
     (message "%s" (apply #'concat (nreverse display-strings)))))
 
 ;; Avy command
-(global-set-key (kbd "M-j") 'avy-goto-char-timer)
+;; (global-set-key (kbd "M-j") 'avy-goto-char-timer)
 
 ;; Kill text
 (defun avy-action-kill-whole-line (pt)
@@ -42,8 +42,8 @@
     (ring-ref avy-ring 0)))
   t)
 
-(setf (alist-get ?k avy-dispatch-alist) 'avy-action-kill-stay
-      (alist-get ?K avy-dispatch-alist) 'avy-action-kill-whole-line)
+;; (setf (alist-get ?k avy-dispatch-alist) 'avy-action-kill-stay
+;;       (alist-get ?K avy-dispatch-alist) 'avy-action-kill-whole-line)
 
 ;; Copy text
 (defun avy-action-copy-whole-line (pt)
@@ -57,8 +57,8 @@
     (ring-ref avy-ring 0)))
   t)
 
-(setf (alist-get ?w avy-dispatch-alist) 'avy-action-copy
-      (alist-get ?W avy-dispatch-alist) 'avy-action-copy-whole-line)
+;; (setf (alist-get ?w avy-dispatch-alist) 'avy-action-copy
+;;       (alist-get ?W avy-dispatch-alist) 'avy-action-copy-whole-line)
 
 ;; Yank text
 (defun avy-action-yank-whole-line (pt)
@@ -66,23 +66,23 @@
   (save-excursion (yank))
   t)
 
-(setf (alist-get ?y avy-dispatch-alist) 'avy-action-yank
-      (alist-get ?Y avy-dispatch-alist) 'avy-action-yank-whole-line)
+;; (setf (alist-get ?y avy-dispatch-alist) 'avy-action-yank
+;;       (alist-get ?Y avy-dispatch-alist) 'avy-action-yank-whole-line)
 
 ;; Transpose/Move text
 (defun avy-action-teleport-whole-line (pt)
   (avy-action-kill-whole-line pt)
   (save-excursion (yank)) t)
 
-(setf (alist-get ?t avy-dispatch-alist) 'avy-action-teleport
-      (alist-get ?T avy-dispatch-alist) 'avy-action-teleport-whole-line)
+;; (setf (alist-get ?t avy-dispatch-alist) 'avy-action-teleport
+;;       (alist-get ?T avy-dispatch-alist) 'avy-action-teleport-whole-line)
 
 ;; Mark text
 (defun avy-action-mark-to-char (pt)
   (activate-mark)
   (goto-char pt))
 
-(setf (alist-get ?  avy-dispatch-alist) 'avy-action-mark-to-char)
+;; (setf (alist-get ?  avy-dispatch-alist) 'avy-action-mark-to-char)
 
 ;; Flyspell words
 (defun avy-action-flyspell (pt)
@@ -95,11 +95,11 @@
   t)
 
 ;; Bind to semicolon (flyspell uses C-;)
-(setf (alist-get ?\; avy-dispatch-alist) 'avy-action-flyspell)
+;; (setf (alist-get ?\; avy-dispatch-alist) 'avy-action-flyspell)
 
 ;; Dictionary: define words
 ;; Replace your package manager or preferred dict package
-(package-install 'dictionary)           
+;; (package-install 'dictionary)
 
 (defun dictionary-search-dwim (&optional arg)
   "Search for definition of word at point. If region is active,
@@ -124,11 +124,11 @@ argument, query for word to search."
    (cdr (ring-ref avy-ring 0)))
   t)
 
-(setf (alist-get ?= avy-dispatch-alist) 'dictionary-search-dwim)
+;; (setf (alist-get ?= avy-dispatch-alist) 'dictionary-search-dwim)
 
 ;; Get Elisp Help
 ;; Replace with your package manager or help library of choice
-(package-install 'helpful)
+;; (package-install 'helpful)
 
 (defun avy-action-helpful (pt)
   (save-excursion
@@ -138,24 +138,24 @@ argument, query for word to search."
    (cdr (ring-ref avy-ring 0)))
   t)
 
-(setf (alist-get ?H avy-dispatch-alist) 'avy-action-helpful)
+;; (setf (alist-get ?H avy-dispatch-alist) 'avy-action-helpful)
 
 ;; Embark
-(package-install 'embark)
+;; (package-install 'embark)
 
 (defun avy-action-embark (pt)
-    (unwind-protect
-        (save-excursion
-          (goto-char pt)
-          (embark-act))
-      (select-window
-       (cdr (ring-ref avy-ring 0))))
-    t)
+  (unwind-protect
+      (save-excursion
+        (goto-char pt)
+        (embark-act))
+    (select-window
+     (cdr (ring-ref avy-ring 0))))
+  t)
 
-(setf (alist-get ?. avy-dispatch-alist) 'avy-action-embark)
+;; (setf (alist-get ?. avy-dispatch-alist) 'avy-action-embark)
 
 ;; Avy + Isearch
-(define-key isearch-mode-map (kbd "M-j") 'avy-isearch)
+;; (define-key isearch-mode-map (kbd "M-j") 'avy-isearch)
 
 ;; Isearch in other windows
 (defun isearch-forward-other-window (prefix)
@@ -178,8 +178,8 @@ argument, query for word to search."
         (isearch-backward)
         (other-window (- next))))))
 
-(define-key global-map (kbd "C-M-s") 'isearch-forward-other-window)
-(define-key global-map (kbd "C-M-r") 'isearch-backward-other-window)
+;; (define-key global-map (kbd "C-M-s") 'isearch-forward-other-window)
+;; (define-key global-map (kbd "C-M-r") 'isearch-backward-other-window)
 
 ;; Google search: requires executable Tuxi
 (defvar google-search-history nil
@@ -227,4 +227,4 @@ active region use it instead."
      (cdr (ring-ref avy-ring 0))))
   t)
 
-(setf (alist-get ?G avy-dispatch-alist) 'avy-action-tuxi)
+;; (setf (alist-get ?G avy-dispatch-alist) 'avy-action-tuxi)
