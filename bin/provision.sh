a8bb50827d7b8523e602f10c250a7e46ec7d5f66
Peter Danenberg <github@pcd.la>
2025-07-04 19:11:12 -0700

    Add bitmap font support and Fixed6x13 configuration
    - Add font configuration section to provision.sh to enable bitmap fonts
    - Remove conflicting fontconfig files that disable bitmapped fonts
    - Create symlink for 70-yes-bitmaps.conf and rebuild font cache
    - Add Fixed6x13 fontconfig alias mapping to Misc Fixed
    - Update alacritty config to use Fixed6x13 font
    - Clean up gitconfig pager setting

--- bin/provision.sh
+++ bin/provision.sh
@@ -16,6 +16,47 @@ else
     echo "No X11 configs found in ~/etc/X11/xorg.conf.d/"
 fi
 
+# Font configuration for bitmapped fonts
+echo "Configuring fonts for bitmapped font support..."
+
+# Remove conflicting font configs if they exist
+FONT_CONF_DIR="/etc/fonts/conf.d"
+CONFLICTING_CONFIGS=(
+    "70-no-bitmaps-and-emoji.conf"
+    "70-no-bitmaps.conf"
+    "70-no-bitmaps-except-emoji.conf"
+    "10-scale-bitmap-fonts.conf"
+)
+
+for config in "${CONFLICTING_CONFIGS[@]}"; do
+    if [ -e "$FONT_CONF_DIR/$config" ]; then
+        echo "Removing conflicting font config: $config"
+        sudo rm -f "$FONT_CONF_DIR/$config"
+    fi
+done
+
+# Create symlink for bitmap font support
+BITMAP_CONF="70-yes-bitmaps.conf"
+BITMAP_SOURCE="/usr/share/fontconfig/conf.avail/$BITMAP_CONF"
+BITMAP_TARGET="$FONT_CONF_DIR/$BITMAP_CONF"
+
+if [ -f "$BITMAP_SOURCE" ]; then
+    if [ ! -L "$BITMAP_TARGET" ]; then
+        echo "Creating symlink for bitmap font support..."
+        sudo ln -sf "$BITMAP_SOURCE" "$BITMAP_TARGET"
+        echo "Bitmap font support enabled"
+    else
+        echo "Bitmap font symlink already exists"
+    fi
+else
+    echo "Warning: $BITMAP_SOURCE not found - bitmap font support may not be available"
+fi
+
+# Rebuild font cache
+echo "Rebuilding font cache..."
+sudo fc-cache -f -v > /dev/null 2>&1
+echo "Font cache rebuilt"
+
 # Other system configs as needed
 # sudo cp ~/etc/dictd.conf /etc/dictd.conf
 
