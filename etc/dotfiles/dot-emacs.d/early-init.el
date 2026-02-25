26f5435f54690e6417083e3cdde046273bc4c37e
Peter Danenberg <github@pcd.la>
2026-02-25 12:39:20 -0800

    fix: move treesit mismatch warning suppression to early-init.el

--- etc/dotfiles/dot-emacs.d/early-init.el
+++ etc/dotfiles/dot-emacs.d/early-init.el
@@ -3,4 +3,9 @@
 ;; Disable package.el in favor of Elpaca
 (setq package-enable-at-startup nil)
 
+;; Suppress tree-sitter font-lock mismatch warnings (grammar vs major mode
+;; version skew in Emacs 31 dev builds). The affected font-lock features
+;; degrade gracefully; this just prevents the *Warnings* buffer from popping up.
+(setq warning-suppress-types '((treesit-font-lock-rules-mismatch)))
+
 ;;; early-init.el ends here
